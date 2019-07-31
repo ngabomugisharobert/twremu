610a9f6f12ab095027ca858977bffcfeee93d3b4
ngabomugisha <ngabo.mugisha.bro@gmail.com>
2019-07-31 14:42:36 +0200

    Revert "tester exit "

--- src/wr-tester.py
+++ src/wr-tester.py
@@ -6,7 +6,6 @@ import time
 # define globals
 mq_connect='localhost'
 situation=[]
-check = True
 
 def start():
 	global channel
@@ -24,7 +23,7 @@ def start():
 		situation.append( { "ItemCode": itemCode, "StationSequenceNumber": None } )
 
 	# Send first message
-	nextStep(check)
+	nextStep()
 
 def forward(x, nextSeqNbr):
 	global channel
@@ -107,9 +106,8 @@ def forward(x, nextSeqNbr):
 		situation.remove(match)
 	return None
 
-def nextStep(check):
+def nextStep():
 	global situation
-	if check == False:sys.exit()
 
 	# find highest possible entry candidate to the wrapping line
 	candidates=[]
@@ -134,14 +132,13 @@ def nextStep(check):
 		match = next((x for x in candidates if x["StationSequenceNumber"]==nextSeqNbr), None)
 		if match is None:
 			forward(i, nextSeqNbr)
-			return True
-	return False
+			break
+		#return None
 
 
 def callback(ch, method, properties, body):
 	global channel
 	global situation
-	global check
 
 	print("callback")
 	reply=json.loads(body)
@@ -158,27 +155,12 @@ def callback(ch, method, properties, body):
 
 	# TODO Checks
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	nextStep()
-<<<<<<< Updated upstream
-=======
-	queue="wr-tester"
->>>>>>> Stashed changes
-=======
-	nextStep(check)
 
->>>>>>> Stashed changes
 	# check for exit
 	if not situation:
-		channel.queue_purge(queue = queue)
-=======
-
-
-	if not nextStep():
->>>>>>> Stashed changes
 		print("Work is done. Bye!")
-		check=False
+		sys.exit()
 
 # Start of initialization
 print('Tips-Wrapline-Tester starting')
